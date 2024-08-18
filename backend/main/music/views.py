aba156a07f283260e0c828c4926ff5890e1fa433
Noe Trevino <noetrevino.music@gmail.com>
2024-08-17 22:07:36 -0500

    showing krish what a dictionary is
    - used person based dictionary
    - this is to show him commits have a body!

--- backend/main/music/views.py
+++ backend/main/music/views.py
@@ -3,7 +3,21 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def helloWorld(request):
-    item = {
-        'message': 'hello world!'
+    employees = {
+        1: {
+            "firstName": "Krish",
+            "lastName": "Shahidadpury",
+            "age": 24
+            },
+        2: {
+            "firstName": "Noe",
+            "lastName": "Trevino",
+            "age": 24
+            }
     }
-    return Response(item)
+    krish = employees[1]
+    # value = krish.get('') < 10
+    return Response(krish.get("age"))
+
+
+
