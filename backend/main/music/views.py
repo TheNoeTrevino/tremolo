fa0d07834f50933024425223e3be3ae4ceb3dd1a
Noe Trevino <noetrevino.music@gmail.com>
2024-08-19 14:35:04 -0500

    test merge commit

--- backend/main/music/views.py
+++ backend/main/music/views.py
@@ -1,23 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+@api_view(["GET"])
 def helloWorld(request):
     employees = {
-        1: {
-            "firstName": "Krish",
-            "lastName": "Shahidadpury",
-            "age": 24
-            },
-        2: {
-            "firstName": "Noe",
-            "lastName": "Trevino",
-            "age": 24
-            }
+        1: {"firstName": "Krish", "lastName": "Shahidadpury", "age": 24},
+        2: {"firstName": "Noe", "lastName": "Trevino", "age": 24},
     }
-    krish = employees[1]
-    # value = krish.get('') < 10
-    return Response(krish.get("age"))
-
-
-
+    return Response(employees)
