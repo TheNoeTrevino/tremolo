e0125e7aa0347eb497a0671d236b3f14e67042cd
Noe Trevino <noetrevino.music@gmail.com>
2024-08-19 19:03:45 -0500

    adjust endpoint to use midi
    - use tempfile to send midi in a 'sendable' format

--- backend/main/music/views.py
+++ backend/main/music/views.py
@@ -1,10 +1,22 @@
-from rest_framework.response import Response
+# from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.http import HttpResponse
+from .testmidi import  marry_had #use relative imports or else an error appears
+import os, tempfile
 
 @api_view(["GET"])
-def helloWorld(request):
-    employees = {
-        1: {"firstName": "Krish", "lastName": "Shahidadpury", "age": 24},
-        2: {"firstName": "Noe", "lastName": "Trevino", "age": 24},
-    }
-    return Response(employees)
+def test_midi(request):
+    midi_stream = marry_had()
+        
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mid") as tmp_file:
+        midi_stream.write('midi', fp=tmp_file.name)
+        tmp_file_path = tmp_file.name
+
+    with open(tmp_file_path, 'rb') as f:
+        midi_bytes = f.read()
+
+    os.remove(tmp_file_path)
+
+    response = HttpResponse(midi_bytes, content_type='audio/midi')
+    response['Content-Disposition'] = 'attachment; filename="test.mid"'
+    return response
