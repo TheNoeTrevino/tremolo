59d99efe162b6b44889cec1626e42fac0edaabbf
Noe Trevino <noetrevino.music@gmail.com>
2024-10-18 16:57:21 -0500

    add: backend endpoint for note game

--- backend/main/music/views.py
+++ backend/main/music/views.py
@@ -3,7 +3,7 @@ from rest_framework.request import Request
 from rest_framework.views import Response
 from django.http import HttpResponse
 
-from .library import get_notes
+from .library import get_notes, note_game
 from .dynamic_mary import DiatonicInformation
 from rest_framework import status
 
@@ -38,6 +38,29 @@ def get_random_notes(request: Request):
 
         try:
             music = get_notes(type, variant, tone)
+
+        # out of the twelve tone system
+        except Exception as e:
+            e = str(e)
+            return Response(
+                f"something is not right!{e}",
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        # TODO: move this to a service
+        response = HttpResponse(music, content_type="application/xml")
+        response["Content-Disposition"] = 'attachment; filename="test.xml"'
+        return response
+
+
+@api_view(["POST"])
+def get_note_game(request: Request):
+    if request.method == "POST":
+        scale: str = request.data.get("scale")  # type: ignore
+        octave: str = request.data.get("octave")  # type: ignore
+
+        try:
+            music = note_game(scale, octave)
 
         # out of the twelve tone system
         except Exception as e:
