cc7304ab8afcfad0f746606d91169c1232f49b2c
TheNoeTrevino <noetrevino.music@gmail.com>
2024-10-25 05:51:38 -0500

    ui: flip the octave options, more intuitive

--- frontend/src/pages/music-display/MusicalOptions.tsx
+++ frontend/src/pages/music-display/MusicalOptions.tsx
@@ -14,16 +14,17 @@ export const scaleOptions: { name: string; option: string }[] = [
 ];
 
 export const octaveOptions: { name: string; option: string }[] = [
-  { name: "1", option: "1" },
-  { name: "2", option: "2" },
-  { name: "3", option: "3" },
-  { name: "4", option: "4" },
-  { name: "5", option: "5" },
-  { name: "6", option: "6" },
-  { name: "7", option: "7" },
-  { name: "8", option: "8" },
   { name: "9", option: "9" },
+  { name: "8", option: "8" },
+  { name: "7", option: "7" },
+  { name: "6", option: "6" },
+  { name: "5", option: "5" },
+  { name: "4", option: "4" },
+  { name: "3", option: "3" },
+  { name: "2", option: "2" },
+  { name: "1", option: "1" },
 ];
+
 // TODO: make the 'name' an icon instead
 export const sixteenthOptions: { name: string; option: string }[] = [
   { name: "1111", option: "1111" },
@@ -32,8 +33,39 @@ export const sixteenthOptions: { name: string; option: string }[] = [
   { name: "211", option: "211" },
   { name: "0111", option: "0111" },
 ];
+
 export const eightOptions: { name: string; option: string }[] = [
   { name: "11", option: "11" },
   { name: "01", option: "01" },
   { name: "10", option: "10" },
+];
+
+//NOTE: these next three items are for the note game, the redundancy is probably
+//needed
+export const sharpOptions: { name: string; value: string }[] = [
+  { name: "C#", value: "CSharp" },
+  { name: "D#", value: "DSharp" },
+  { name: "E#", value: "ESharp" },
+  { name: "F#", value: "FSharp" },
+  { name: "G#", value: "GSharp" },
+  { name: "A#", value: "ASharp" },
+  { name: "B#", value: "BSharp" },
+];
+export const naturalOptions: { name: string; value: string }[] = [
+  { name: "C", value: "C" },
+  { name: "D", value: "D" },
+  { name: "E", value: "E" },
+  { name: "F", value: "F" },
+  { name: "G", value: "G" },
+  { name: "A", value: "A" },
+  { name: "B", value: "B" },
+];
+export const flatOptions: { name: string; value: string }[] = [
+  { name: "Cb", value: "CFlat" },
+  { name: "Db", value: "DFlat" },
+  { name: "Eb", value: "EFlat" },
+  { name: "Fb", value: "FFlat" },
+  { name: "Gb", value: "GFlat" },
+  { name: "Ab", value: "AFlat" },
+  { name: "Bb", value: "BFlat" },
 ];
