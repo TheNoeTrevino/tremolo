584d1aab3cdfdebb049ac406b60e24762dda95b9
TheNoeTrevino <noetrevino.music@gmail.com>
2024-10-24 23:53:59 -0500

    setup: auth0, env vars, package.json

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { Box, createTheme, ThemeProvider } from "@mui/material";
 import Footer from "./components/Footer";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { getAuthDomain, getClientID } from "./Secret";
 
 // https://colorhunt.co/palette/1e201e3c3d37697565ecdfcc
 const theme = createTheme({
@@ -17,21 +19,29 @@ const theme = createTheme({
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "100vh",
-        }}
-      >
-        <Navbar />
-        <div id="detail">
-          <Outlet />
-        </div>
-        <Footer />
-      </Box>
-    </ThemeProvider>
+    <Auth0Provider
+      domain={getAuthDomain()}
+      clientId={getClientID()}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100vh",
+          }}
+        >
+          <Navbar />
+          <div id="detail">
+            <Outlet />
+          </div>
+          <Footer />
+        </Box>
+      </ThemeProvider>
+    </Auth0Provider>
   );
 }
 export default App;
