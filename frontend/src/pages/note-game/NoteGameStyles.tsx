e1f3071cf35e1e7c2f1ba8e953fb94d605aa3291
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-01 12:10:15 -0500

    move: styles into their own file

---
+++ frontend/src/pages/note-game/NoteGameStyles.tsx
@@ -0,0 +1,50 @@
+const noteGameStyles = {
+  scoreboard: {
+    width: "33%",
+    height: "3rem",
+    mb: "2rem",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+
+  mainDiv: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+
+  musicDisplay: {
+    display: "flex",
+    width: "66%",
+    height: "20rem",
+    alignItems: "center",
+    mb: "2rem",
+  },
+
+  optionButtonsCard: {
+    mb: "2rem",
+    display: "flex",
+    flexDirection: "column",
+    height: "20rem",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  optionButtonsContainer: {
+    display: "flex",
+    flexDirection: "column",
+    mx: "1rem",
+  },
+
+  answerButtons: {
+    m: "1rem",
+    textTransform: "none",
+    width: "9rem",
+    height: "3rem",
+  },
+};
+
+export { noteGameStyles };
