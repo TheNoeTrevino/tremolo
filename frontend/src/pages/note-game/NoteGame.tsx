19f6613494b9353fd2bf9c77148c55889f18f72c
TheNoeTrevino <noetrevino.music@gmail.com>
2024-10-31 23:41:46 -0500

    add: counter for score, make promise a string

--- frontend/src/pages/note-game/NoteGame.tsx
+++ frontend/src/pages/note-game/NoteGame.tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, Card } from "@mui/material";
 import { useState, MouseEvent, useEffect } from "react";
 import MusicButton from "../music-display/MusicButton";
 import {
@@ -11,11 +11,16 @@ import {
 import { MusicService } from "../../services/MusicService";
 import { musicButtonStyles } from "../../styles";
 
+// TODO: update the score everytime the user inputs something, use in useEffect
+// dependency
 const NoteGame = () => {
-  //TODO: use effect to return the value, then immediately call another request
-  //for speed!!!!!!!!!!
+  const [totalCounter, setTotalcounter] = useState<number>(0);
+  const [correctCounter, setCorrectCounter] = useState<number>(0);
+  const [incorrectCounter, setIncorrectCounter] = useState<number>(0);
   const [scaleChoice, setScale] = useState<string>("C");
   const [octaveChoice, setOctaveChoice] = useState<string>("4");
+  const [noteName, setNoteName] = useState<string>("C");
+
   const [scaleAnchorEl, setScaleAnchorEl] = useState<null | HTMLElement>(null);
   const [octaveAnchorEl, setOctaveAnchorEl] = useState<null | HTMLElement>(
     null,
@@ -38,65 +43,122 @@ const NoteGame = () => {
   const chooseOctave = (octaveChoice: string) => {
     setOctaveChoice(octaveChoice);
   };
+  async function fetchNote(): Promise<void> {
+    setNoteName(await MusicService.getNoteGameXml(scaleChoice, octaveChoice));
+  }
   const openScaleOptions = Boolean(scaleAnchorEl);
   const openOctaveOptions = Boolean(octaveAnchorEl);
   const totalOptions = [sharpOptions, naturalOptions, flatOptions];
   useEffect(() => {
-    MusicService.getNoteGameXml(scaleChoice, octaveChoice);
-  }, [scaleChoice, octaveChoice]);
-  const validateInput = (noteName: string): void => {
-    console.log(noteName);
+    fetchNote();
+  }, [scaleChoice, octaveChoice, totalCounter]);
+
+  const validateInput = (noteKey: string): void => {
+    setTotalcounter(totalCounter + 1);
+    if (noteKey != noteName) {
+      setIncorrectCounter(correctCounter + 1);
+      alert("false");
+      return;
+    }
     //TODO: make the function check if they are equal, if they are not, make the
     //button red for a moment, if they are equal, make it green for a moment
-    return;
+    setCorrectCounter(correctCounter + 1);
+    alert("true");
   };
+
   return (
     <>
       <Box
-        id="sheet-music-div"
+        my={"2rem"}
         sx={{
-          width: "100%",
-          height: "500px",
-          border: "1px dashed grey",
-          alignContent: "center",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
         }}
-      ></Box>
-      <MusicButton
-        text="Choose Scale"
-        handleClick={handleScaleClick}
-        options={scaleOptions}
-        anchorEl={scaleAnchorEl}
-        open={openScaleOptions}
-        handleClose={handleScaleClose}
-        handleOptionClick={chooseScale}
-        styles={musicButtonStyles}
-      />
-      <MusicButton
-        text="Choose Octave"
-        handleClick={handleOctaveClick}
-        options={octaveOptions}
-        anchorEl={octaveAnchorEl}
-        open={openOctaveOptions}
-        handleClose={handleOctaveClose}
-        handleOptionClick={chooseOctave}
-        styles={musicButtonStyles}
-      />
-      <Box>
-        {totalOptions.map((optionList) => (
-          <Box>
-            {optionList.map((option) => (
-              // TODO: make the buttons larger for the kids
-              <Button
-                key={option.value}
-                variant="contained"
-                sx={{ m: 1 }}
-                onClick={() => validateInput(option.value)}
-              >
-                {option.name}
-              </Button>
-            ))}
-          </Box>
-        ))}
+      >
+        <Card
+          elevation={4}
+          sx={{
+            width: "33%",
+            height: "2rem",
+            textAlign: "center",
+            textJustify: "center",
+            mb: "2rem",
+          }}
+        >
+          {correctCounter / totalCounter} %
+        </Card>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <Card
+            id="sheet-music-div"
+            elevation={6}
+            sx={{
+              display: "flex",
+              width: "66%",
+              height: "20rem",
+              alignItems: "center",
+              mb: "2rem",
+            }}
+          ></Card>
+          <Card
+            elevation={6}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              height: "20rem",
+              justifyContent: "center",
+            }}
+          >
+            <Box>
+              <MusicButton
+                text="Choose Scale"
+                handleClick={handleScaleClick}
+                options={scaleOptions}
+                anchorEl={scaleAnchorEl}
+                open={openScaleOptions}
+                handleClose={handleScaleClose}
+                handleOptionClick={chooseScale}
+                styles={musicButtonStyles}
+              />
+              <MusicButton
+                text="Choose Octave"
+                handleClick={handleOctaveClick}
+                options={octaveOptions}
+                anchorEl={octaveAnchorEl}
+                open={openOctaveOptions}
+                handleClose={handleOctaveClose}
+                handleOptionClick={chooseOctave}
+                styles={musicButtonStyles}
+              />
+            </Box>
+          </Card>
+        </Box>
+        <Box>
+          {totalOptions.map((optionList) => (
+            <Box>
+              {optionList.map((option) => (
+                // TODO: make the buttons larger for the kids
+                <Button
+                  key={option.value}
+                  variant="contained"
+                  sx={{ m: 1, textTransform: "none" }}
+                  onClick={() => validateInput(option.value)}
+                >
+                  {option.name}
+                </Button>
+              ))}
+            </Box>
+          ))}
+        </Box>
       </Box>
     </>
   );
