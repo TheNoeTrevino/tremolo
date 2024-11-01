8d0b97f80800c87ee35b5f09aadd7dc0dd533823
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-01 14:10:48 -0500

    styles: reorganize, better practice

--- frontend/src/pages/note-game/NoteGameStyles.tsx
+++ frontend/src/pages/note-game/NoteGameStyles.tsx
@@ -1,19 +1,20 @@
 const noteGameStyles = {
   scoreboard: {
-    width: "33%",
-    height: "3rem",
-    mb: "2rem",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
+    width: "33%",
+    height: "3rem",
+    mb: "2rem",
   },
 
   mainDiv: {
-    width: "100%",
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-evenly",
     alignItems: "center",
+    width: "100%",
+    mb: "2rem",
   },
 
   musicDisplay: {
@@ -21,11 +22,9 @@ const noteGameStyles = {
     width: "66%",
     height: "20rem",
     alignItems: "center",
-    mb: "2rem",
   },
 
   optionButtonsCard: {
-    mb: "2rem",
     display: "flex",
     flexDirection: "column",
     height: "20rem",
