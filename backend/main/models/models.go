0d3109c7eb98e7779393d7803f2575494008f875
Noe Trevino <noetrevino.music@gmail.com>
2024-11-14 01:19:15 -0600

    FEAT: first custom validation

--- backend/main/models/models.go
+++ backend/main/models/models.go
@@ -39,3 +39,60 @@ const (
 	Parent  Role = "PARENT"
 	Student Role = "STUDENT"
 )
+
+// TODO: make each model its own file so we can show the validations on each
+// file. also make these return a custom error
+//
+// NOTE: these are all user validations
+
+// youtube custom validation
+func validateUserRole(fl validator.FieldLevel) bool {
+	switch fl.Field().String() {
+	case "TEACHER", "STUDENT", "ADMIN":
+		return true
+	}
+
+	return false
+}
+
+func (user *User) ValidateUser() error {
+	validate := validator.New()
+	// do not forget to register your custom validations
+	validate.RegisterValidation("role", validateUserRole)
+
+	err := validate.Struct(user)
+	if err != nil {
+		if errs, ok := err.(validator.ValidationErrors); ok {
+			for _, fieldErr := range errs {
+				switch fieldErr.StructField() {
+				case "FirstName":
+					switch fieldErr.Tag() {
+					case "required":
+						return errors.New("First name is required")
+					case "alpha":
+						return errors.New("First name must be only alphabetical charaters")
+					}
+
+				case "LastName":
+					switch fieldErr.Tag() {
+					case "required":
+						return errors.New("Last name is required")
+					case "alpha":
+						return errors.New("Last name must be only alphabetical charaters")
+					}
+
+				case "Role":
+					switch fieldErr.Tag() {
+					case "required":
+						return errors.New("Role is required when making a user")
+					case "role":
+						return errors.New("Role must be either STUDENT, TEACHER, or ADMIN")
+					}
+				}
+			}
+		}
+		return err
+	}
+
+	return nil
+}
