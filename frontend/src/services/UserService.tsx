cc9309399e78d82eb545f5d76ee41ace9196d10a
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-15 02:20:25 -0600

    rename: serivce -> snake case

--- frontend/src/services/UserService.tsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-//TODO: switch all axios to normal fetch and learn how that shit works
-
-export class UserService {
-  async getAllUsers(): Promise<void> {
-    try (
-    const response = await fetch("http://localhost:5000/students", )
-  
-}
