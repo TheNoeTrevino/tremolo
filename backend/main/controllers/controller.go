f81c59ee7a899f62953e7ab51d0cbc19614c75d9
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-15 02:19:32 -0600

    reorganize: routes

---
+++ backend/main/controllers/controller.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"sight-reading/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupTeacherRoutes(router *gin.Engine) {
+	router.GET("/teachers", services.GetTeachers)
+	router.GET("/teacher/:id", services.GetTeacher)
+	router.GET("/students", services.GetStudents)
+	router.GET("/student/:id", services.GetStudent)
+	router.POST("/user", services.CreateUser)
+}
