f0778b888b91539c8c088016632c855197ba831d
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-14 23:03:00 -0600

    add: tests

--- backend/main/tests/user_test.go
+++ backend/main/tests/user_test.go
@@ -1,14 +1,14 @@
 package tests
 
 import (
-	"sight-reading/models"
+	dtos "sight-reading/DTOs"
 	"testing"
 )
 
 // TODO: validate the error messages
 // Happy path
-func TestHappyCheckValidation(t *testing.T) {
-	user := &models.User{
+func TestHappyUserCheckValidation(t *testing.T) {
+	user := &dtos.User{
 		FirstName: "Noe",
 		LastName:  "Trevino",
 		Role:      "TEACHER",
@@ -20,18 +20,77 @@ func TestHappyCheckValidation(t *testing.T) {
 	}
 }
 
+func TestHappyEntryCheckValidation(t *testing.T) {
+	entry := &dtos.Entry{
+		TimeLength:       "01:30:30",
+		Questions:        12,
+		CorrectQuestions: 11,
+		UserID:           4,
+	}
+
+	err := entry.ValidateEntry()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestHappyDistrictCheckValidation(t *testing.T) {
+	district := &dtos.District{
+		Title:   "Byron Nelson High School",
+		County:  "Denton",
+		State:   "Texas",
+		Country: "USA",
+	}
+
+	err := district.ValidateDistrict()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 // Sad path
-func TestSadCheckValidation(t *testing.T) {
-	user := &models.User{
-		FirstName: "Noe",
-		LastName:  "Trevino",
+func TestSadUserCheckValidation(t *testing.T) {
+	user := &dtos.User{
+		FirstName: "Noe1",
+		LastName:  "Trevino12",
 		Role:      "STUDEN",
 	}
 
 	err := user.ValidateUser()
 	if err == nil {
-		t.Fatal(err)
+		t.Fatal("There are no errors dectected")
 	} else {
-		t.Logf("failed as expected: %v", err)
+		t.Logf("Failed as expected: %v", err)
+	}
+}
+
+func TestSadEntryCheckValidation(t *testing.T) {
+	entry := &dtos.Entry{
+		TimeLength:       "20:30:90",
+		Questions:        12,
+		CorrectQuestions: 19,
+		UserID:           4,
+	}
+	err := entry.ValidateEntry()
+	if err == nil {
+		t.Fatal("There are no errors dectected, this is a sad path")
+	} else {
+		t.Logf("Failed as expected: %v", err)
+	}
+}
+
+func TestSadDistrictCheckValidation(t *testing.T) {
+	district := &dtos.District{
+		// Title:   "Byron Nelson High School",
+		// County:  "Denton",
+		// State:   "Texas",
+		Country: "USA",
+	}
+
+	err := district.ValidateDistrict()
+	if err == nil {
+		t.Fatal("There are no errors dectected, this is a sad path")
+	} else {
+		t.Logf("Failed as expected: %v", err)
 	}
 }
