cdb625d6d4aa5c23e6edbc44eb3f33a4be2a6f98
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-15 01:29:40 -0600

    add: user service

---
+++ frontend/src/services/UserService.tsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+//TODO: switch all axios to normal fetch and learn how that shit works
+
+export class UserService {
+  async getAllUsers(): Promise<void> {
+    try (
+    const response = await fetch("http://localhost:5000/students", )
+  
+}
