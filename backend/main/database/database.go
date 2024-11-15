d620b738948af3930ac2c869871d337eca404ee8
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-15 01:29:13 -0600

    change: database url

--- backend/main/database/database.go
+++ backend/main/database/database.go
@@ -8,7 +8,7 @@ import (
 var DBClient *sqlx.DB
 
 func InitializeDBConnection() {
-	db, err := sqlx.Open("postgres", "postgres://postgres:test123@localhost:5432/postgres?sslmode=disable")
+	db, err := sqlx.Open("postgres", "postgres://postgres:@localhost:5432/tremolo?sslmode=disable")
 	if err != nil {
 		panic(err.Error())
 	}
