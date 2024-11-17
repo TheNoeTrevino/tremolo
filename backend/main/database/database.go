8d5e5b058ba5081a354ad36f1acca0daaccd79ef
Noe Trevino <noetrevino.music@gmail.com>
2024-11-17 00:40:14 -0600

    add: database connection validatoin

--- backend/main/database/database.go
+++ backend/main/database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"strings"
+
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
@@ -11,6 +13,10 @@ func InitializeDBConnection() {
 	db, err := sqlx.Open("postgres", "postgres://postgres:@localhost:5432/tremolo?sslmode=disable")
 	if err != nil {
 		panic(err.Error())
+	} else {
+		println(strings.Repeat("------------------------------", 2))
+		println("\nConnected to database successfully\n")
+		println(strings.Repeat("------------------------------", 2))
 	}
 
 	err = db.Ping()
