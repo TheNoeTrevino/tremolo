f44ca12256a097f247e2098804143e09fd9f67c2
Noe Trevino <noetrevino.music@gmail.com>
2024-11-17 00:40:01 -0600

    generate: teacher, student
    - missing entry, how can we randomize the regex? probably will just
    - typecast some ints

--- backend/main/generation/data_gen.go
+++ backend/main/generation/data_gen.go
@@ -1,14 +1,25 @@
 package generation
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/manveru/faker"
 )
 
 func GenerateData() {
+	initFaker()
+	println(strings.Repeat("------------------------------", 2))
+	println("\nGenerating data...\n")
+	println(strings.Repeat("------------------------------", 2))
+
 	fake, err := faker.New("en")
 	if err != nil {
 		panic(err)
 	}
-	println(fake.Name())  //> "Adriana Crona"
-	println(fake.Email()) //> charity.brown@fritschbotsford.biz
+	println(fake.CompanyName() + " ISD")
+	println(fake.CompanyName() + " ISD")
+	fmt.Println(GenerateFakeDistrict())
+	fmt.Println(GenerateFakeTeacher())
+	fmt.Println(GenerateFakeStudent())
 }
--- backend/main/generation/fakeDTOs.go
+++ backend/main/generation/fakeDTOs.go
@@ -1,5 +1,12 @@
 package generation
 
+import (
+	"log"
+	"math/rand/v2"
+
+	"github.com/manveru/faker"
+)
+
 type District struct {
 	// ID      *int16 `db:"id"      json:"id"`
 	Title   string
@@ -23,4 +30,50 @@ type Entry struct {
 	Questions        int16
 	CorrectQuestions int16
 	UserID           int16
+}
+
+var fake *faker.Faker
+
+func initFaker() {
+	var err error
+	fake, err = faker.New("en")
+	if err != nil {
+		log.Fatal("Did not instantiate faker")
+		panic(err)
+	}
+}
+
+func GenerateFakeDistrict() District {
+	return District{
+		Title:   fake.FirstName() + " ISD",
+		County:  fake.City(),
+		State:   fake.State(),
+		Country: fake.Country(),
+	}
+}
+
+func GenerateFakeTeacher() User {
+	return User{
+		FirstName:  fake.FirstName(),
+		LastName:   fake.LastName(),
+		Role:       "TEACHER",
+		DistrictID: int16(rand.IntN(100)),
+	}
+}
+
+func GenerateFakeStudent() User {
+	return User{
+		FirstName:  fake.FirstName(),
+		LastName:   fake.LastName(),
+		Role:       "STUDENT",
+		DistrictID: int16(rand.IntN(100)),
+	}
+}
+
+func GenerateFakeEntry() Entry {
+	return Entry{
+		TimeLength:       "",
+		Questions:        int16(rand.IntN(100)),
+		CorrectQuestions: int16(rand.IntN(100)),
+	}
 }
