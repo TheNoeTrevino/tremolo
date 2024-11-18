9e16c400d2b9a0fb68578d16b63e87048cae86f8
Noe Trevino <noetrevino.music@gmail.com>
2024-11-17 20:06:12 -0600

    add: first faker
    - need to add database tags

--- backend/main/generation/fakeDTOs.go
+++ backend/main/generation/fakeDTOs.go
@@ -3,6 +3,7 @@ package generation
 import (
 	"log"
 	"math/rand/v2"
+	"sight-reading/database"
 
 	"github.com/manveru/faker"
 )
@@ -32,6 +33,11 @@ type FakeEntry struct {
 	UserID           int16
 }
 
+type FakeAssociation struct {
+	ParentID int
+	ChildID  int
+}
+
 var fake *faker.Faker
 
 func initFaker() {
@@ -52,20 +58,89 @@ func GenerateFakeDistrict() FakeDistrict {
 	}
 }
 
-func generateFakeTeacher() FakeUser {
-	return FakeUser{
+// FIX: take the teacher generation out, so we can have multiple student to
+// multiple teachers
+func generateFakeTeacherWithStudents() FakeUser {
+	query := `
+  INSERT INTO users (
+    first_name,
+    last_name,
+    district_id,
+    role
+  )
+  VALUES (
+    :first_name,
+    :last_name,
+    :district_id,
+    :role
+  )
+  RETURNING
+    id
+  `
+
+	teacher := FakeUser{
 		FirstName:  fake.FirstName(),
 		LastName:   fake.LastName(),
 		Role:       "TEACHER",
 		DistrictID: int16(rand.IntN(100)),
 	}
+	rows, err := database.DBClient.NamedQuery(query, teacher)
+	if err != nil {
+		log.Panic("teacher was not added to the db", err.Error())
+	}
+
+	var teacherId int
+	if rows.Next() {
+		err := rows.Scan(teacherId)
+		if err != nil {
+			log.Panic("teacher id was not extracted properly", err.Error())
+		}
+	}
+
+	for i := 0; i < 5; i++ {
+		student := generateFakeUser("STUDENT")
+		rows, err := database.DBClient.NamedQuery(query, student)
+		if err != nil {
+			log.Panic("student was not added to the db", err.Error())
+		}
+
+		var studentId int
+		if rows.Next() {
+			err := rows.Scan(studentId)
+			if err != nil {
+				log.Panic("student id was not extracted properly", err.Error())
+			}
+		}
+
+		associationQuery := `
+      INSERT INTO teacherToStudent (
+        teacher_id,
+        user_id,
+      )
+      VALUES (
+        :teacher_id,
+        :user_id,
+      )
+    `
+		associationIds := FakeAssociation{
+			ParentID: teacherId,
+			ChildID:  studentId,
+		}
+
+		rows, err = database.DBClient.NamedQuery(associationQuery, associationIds)
+		if err != nil {
+			log.Panic("association from teacher to student was not added to db", err.Error())
+		}
+	}
+
+	return teacher
 }
 
-func generateFakeStudent() FakeUser {
+func generateFakeUser(role string) FakeUser {
 	return FakeUser{
 		FirstName:  fake.FirstName(),
 		LastName:   fake.LastName(),
-		Role:       "STUDENT",
+		Role:       role,
 		DistrictID: int16(rand.IntN(100)),
 	}
 }
