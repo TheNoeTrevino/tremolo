85da613f6c0cf0fc71ab68d4faafb483e122197e
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-07 22:33:23 -0600

    refactor: moved not logged in -> component

--- frontend/src/pages/users/Profile.tsx
+++ frontend/src/pages/users/Profile.tsx
@@ -1,35 +1,7 @@
-import { Box, Button, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import NotLoggedIn from "./NotLoggedIn";
 
 const Profile = () => {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "5rem",
-        margin: "2rem",
-        alignItems: "center",
-      }}
-    >
-      <Typography variant="h3" textAlign="center">
-        You are not currently logged in!
-      </Typography>
-
-      <Typography variant="h4" textAlign="center">
-        Please Log In Below
-      </Typography>
-      <Button
-        key="login"
-        component={Link}
-        to={"../login"}
-        variant="contained"
-        sx={{ width: "9rem" }}
-      >
-        Log In
-      </Button>
-    </Box>
-  );
+  return <NotLoggedIn />;
 };
 
 export default Profile;
