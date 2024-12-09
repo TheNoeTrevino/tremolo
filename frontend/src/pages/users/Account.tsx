a492240f08e9f2892eb1942459fdae17fdceb841
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-09 15:45:23 -0500

    start: account page

--- frontend/src/pages/users/Account.tsx
+++ frontend/src/pages/users/Account.tsx
@@ -1,5 +1,34 @@
+import { Box, Card, Typography } from "@mui/material";
+
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "2rem",
+        m: "2rem",
+      }}
+    >
+      <Card variant="outlined">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "2rem",
+            padding: "2rem",
+          }}
+        >
+          <Typography>Full Name</Typography>
+          <Typography>School Name</Typography>
+          <Typography>Name</Typography>
+          <Typography>Name</Typography>
+        </Box>
+      </Card>
+      <Card variant="outlined">Photo here </Card>
+    </Box>
+  );
 };
 
 export default Account;
