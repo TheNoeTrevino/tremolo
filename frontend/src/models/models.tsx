85eefbb819e39a2b7bfb3ffa50f698321edcc5d6
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-20 11:54:34 -0600

    add(frontend): user, entry, school DTO

--- frontend/src/models/models.tsx
+++ frontend/src/models/models.tsx
@@ -21,3 +21,33 @@ export interface NoteGameDTO {
   noteName: string;
   noteOctave: string;
 }
+
+export interface UserDTO {
+  firstName: string;
+  lastName: string;
+  age: number;
+  email: string;
+}
+
+// fix the any here with the proper ones
+export interface entryDTO {
+  ID: number;
+  TimeLength: any;
+  CreatedDate: any;
+  CreatedTime: any;
+  TotalQuestions: number;
+  CorrectQuestions: number;
+  NPM: number;
+}
+
+// fix the any here with the proper ones
+export interface schoolDTO {
+  ID: number;
+  Title: string;
+  City: string;
+  County: string;
+  State: string;
+  Country: string;
+  CreatedDate: any;
+  CreatedTime: any;
+}
