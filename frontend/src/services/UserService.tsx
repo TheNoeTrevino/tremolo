848c4492ff92ec9da38938d054e12b9813e07c2b
TheNoeTrevino <noetrevino.music@gmail.com>
2024-12-20 11:53:57 -0600

    add(frontend): get user entries

--- frontend/src/services/UserService.tsx
+++ frontend/src/services/UserService.tsx
@@ -1,3 +1,5 @@
+import { entryDTO } from "../models/models";
+
 export const UserService = {
   async loginUser(username: string, password: string): Promise<boolean> {
     try {
@@ -16,10 +18,30 @@ export const UserService = {
 
       const data = await response.json();
       const isUser = Boolean(data);
+
+      alert(isUser);
       return isUser;
     } catch (error) {
       console.log("Could not validate user");
+      alert("Could not validate user");
       return false;
+    }
+  },
+
+  async getUserEntries(userId: number): Promise<any> {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/entries/${userId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data: entryDTO[] = await response.json();
+      console.log(data);
+    } catch (error) {
+      alert("User entries were not fetched from database as expected");
+      console.log(error);
     }
   },
 };
