283fe77d2d3e05082fdaea25c2c1cbaf7fad8469
TheNoeTrevino <noetrevino.music@gmail.com>
2025-11-02 09:22:03 -0800

    security(cors): allow localhost only (for now)
    - make this an environment variable

--- backend/main/main.go
+++ backend/main/main.go
@@ -6,12 +6,16 @@ import (
 	"sight-reading/controllers"
 	"sight-reading/database"
 	"sight-reading/generation"
+	"sight-reading/middleware"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// init global deps
 	database.InitializeDBConnection()
+	middleware.InitJWTSecret()
 
 	// faker flag
 	runPackage := flag.Bool("fake-it", false, "use this flag to generate data")
@@ -22,6 +26,15 @@ func main() {
 
 	router := gin.Default()
 
+	// cors
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:5173"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
+	config.AllowCredentials = true
+	router.Use(cors.New(config))
+
+	controllers.SetupAuthRoutes(router)
 	controllers.SetupTeacherRoutes(router)
 
 	err := router.Run(":5001")
